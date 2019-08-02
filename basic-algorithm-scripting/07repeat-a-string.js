12b37ea0379e0256a1ecb14fd4d871c4afec968b
Kinga Polgar <34431942+kpolgar@users.noreply.github.com>
2019-08-02 07:43:19 -0400

    Create 07repeat-a-string.js

---
+++ basic-algorithm-scripting/07repeat-a-string.js
@@ -0,0 +1,26 @@
+/*Basic Algorithm Scripting: Repeat a String Repeat a String
+Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.*/
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  let total = ""
+  console.log(total)
+  for (let i = 0; i < num; i++) {
+    
+    total = total + str
+    console.log(total);
+  }
+    return total;
+  }
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  let total = ""
+  console.log(total)
+  for (let i = 0; i < num; i++) {
+    
+    total = total + str
+    console.log(total);
+  }
+    return total;
+  }
